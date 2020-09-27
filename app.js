555281fd9357964d0acffc02f7c1fb1a8e3e5f76
SriKutty962 <officialkuttysri@gmail.com>
2020-09-27 14:23:56 +0530

    minor changes

--- app.js
+++ app.js
@@ -166,14 +166,14 @@ app.post("/login", function (req, res) {
 });
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//     port = 3000;
+// }
 
 
 
 
-app.listen(port, function () {
-    console.log("Server started ");
+app.listen(3000, function () {
+    console.log("Server started at port 3000 ");
 });
